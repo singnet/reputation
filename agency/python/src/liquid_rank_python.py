cfacaeab84794fbfca27b2c6947aae67e28c253d
nejc9921 <33785967+nejc9921@users.noreply.github.com>
2018-11-14 18:58:55 +0100

    Code actually ran on server
    So, the newest code, with most commenting done.

--- agency/python/src/liquid_rank_python.py
+++ agency/python/src/liquid_rank_python.py
@@ -68,9 +68,12 @@ def reputation_calc_p1(new_subset,first_occurance):
     del(sorted_merge)
     return(new_array,dates_array,to_array,first_occurance)
 
+### Here we update add new records to our dictionary if new reputation is detected. We assume that we start with value of 0.1,
+### but this could be adjusted.
 def update_reputation(reputation,new_array):
     i = 0
     while i<len(new_array):
+        
         if new_array[i][0] in reputation:
             pass
         else:
@@ -83,7 +86,7 @@ def update_reputation(reputation,new_array):
     return(reputation)
 
 def calculate_new_reputation(new_array,to_array,reputation):
-    ### This is needed;
+    ### This is needed; we calculate reputations in our timeframe in this specific function.
     mys = {}
     i = 0
     while i<len(new_array):
@@ -92,8 +95,7 @@ def calculate_new_reputation(new_array,to_array,reputation):
         else:
             mys[new_array[i][1]] = 0
         i+=1
-
-    ### Problem!!! All results are the same!!!
+    ### We use formula where we make sure the growth of reputation won't be too high.
     unique_ids = np.unique(to_array)
     k=0
     i = 0
@@ -108,20 +110,23 @@ def calculate_new_reputation(new_array,to_array,reputation):
 
         i+=1
 
-    ### nr 5.
+    ### nr 5. in paper on how to calculate the reputation.
     for k in mys.keys():
         if mys[k]<0:
             mys[k] = -np.log10(1 - mys[k])
         else:
             mys[k] = np.log10(1 + mys[k])
-    #print(mys)
     ### Nr 6;
     max_value = max(mys.values())
     for k in mys.keys():
         mys[k] = mys[k] /max_value
 
     return(mys)
+
+### Below we have 3 different proposals on how to update reputations. They mainly differ in the speed of decay. Only
+### one is generally used when calculating the updates.
 def update_reputation_approach_a(reputation,mys,since,date_zero,our_date):
+    ### Define times.
     since_datezero = since - date_zero
     date_since = our_date - since
     since_datezero = since_datezero.days
@@ -149,82 +154,93 @@ def update_reputation_approach_b(reputation,mys):
 def update_reputation_approach_c(first_occurance,reputation,mys,since,our_date):
     ### 3rd approach or c)     
     date_since = (our_date - since).days
+    print("Here we are")
     j = 0
 
+    all_keys = set(mys.keys())
     for k  in reputation.keys():
         since_datebirth = first_occurance[k]
         date_datebirth = since_datebirth + date_since
         if date_datebirth==0:
             date_datebirth = 1
-        if k in mys.keys():    
+        if k in all_keys:    
             reputation[k] = reputation[k] * since_datebirth + mys[k] * date_since
             reputation[k] = reputation[k]/date_datebirth 
         else:
             reputation[k] = (reputation[k] * since_datebirth)/date_datebirth   
-        if j%1000==0:
-
-            print(j)        
-
         j+=1        
     return(reputation)
 
+### Save zipped file
 def save_zipped_pickle(obj, filename, protocol=-1):
     with gzip.open(filename, 'wb') as f:
         pickle.dump(obj, f, protocol)
-
+### Load zipped file.
 def load_zipped_pickle(filename):
     with gzip.open(filename, 'rb') as f:
         loaded_object = pickle.load(f)
         return loaded_object
-    
+### Main script.    
 def run_script():    
+    ### First we just define the dictionaries.
     first_occurance = dict()
     reputation = dict()
     previous_reputation = dict()
+    ### Start of our period
     date_zero = datetime(2015, 7, 30, 0, 0)
-
-    filepath = os.path.join('data', 'January_2018_new.zip')
-    filepath
-    start1 = time.clock()
-    with zipfile.ZipFile(filepath) as z:
-        for filename in z.namelist():
-            print(filename)
-            daily_data = []
-            mydate = find_between_r(filename,"ethereum_",".tsv")
-            our_date = datetime.strptime(mydate, '%Y-%m-%d')
-            since = our_date - timedelta(days=1)
-            with z.open(filename) as f:
-                lines = [x.decode('utf8').strip() for x in f.readlines()]
-                for line in lines:
-                    line = line.replace("\t",",")
-                    daily_data.append(line)    
-            i=0
-            while i < len(daily_data):
-                daily_data[i] = daily_data[i].split(',' )
-                i+=1        
-
-            daily_data = pd.DataFrame(daily_data)
-            daily_data.columns = ['Coin','Timestamp','Transfer','From','To','Amount']
-            daily_data = daily_data[daily_data['Transfer']=='transfer']
-            del(daily_data['Coin'])
-            del(daily_data['Transfer'])
-            daily_data = daily_data.reset_index()
-            daily_data['Amount'] = pd.to_numeric(daily_data['Amount'], errors='ignore')
-            array1 , dates_array, to_array, first_occurance = reputation_calc_p1(daily_data,first_occurance)
-            del(daily_data)
-            reputation = update_reputation(reputation,array1)
-
-            new_reputation = calculate_new_reputation(array1,to_array,reputation)
-
-            reputation = update_reputation_approach_c(first_occurance,reputation,new_reputation,since,our_date)
-            savefile = "reputation_"+str(our_date)[0:10]+".data"
-            fp=gzip.open(savefile,'wb')
-            pickle.dump(reputation,fp)
-            fp.close()
-    print("Ending time is:",time.clock()-start)
-    savefile = "first_occurance_all.data"
-    fp=gzip.open(savefile,'wb')
-    pickle.dump(first_occurance,fp)
-run_script()    
     
+    folder="data"
+    start = timeit.timeit()
+    ### Get each filename (sort them first) and read it.
+    for filename in np.sort(os.listdir(folder)):
+        filepath = os.path.join(folder, filename)
+        print(filename)
+        daily_data = []
+        mydate = find_between_r(filename,"ethereum_",".tsv")
+        ### Get dates. Since means what was day before because we update ranking every day.
+        our_date = datetime.strptime(mydate, '%Y-%m-%d')
+        since = our_date - timedelta(days=1)
+        with open(filepath) as f:
+            lines = [x for x in f.readlines()]
+            for line in lines:
+                line = line.replace("\t",",")
+                daily_data.append(line)    
+        i=0
+        while i < len(daily_data):
+            daily_data[i] = daily_data[i].split(',')
+            i+=1    
+    
+        ### All data is now in daily_data. We add columns to the file.
+        daily_data = pd.DataFrame(daily_data)
+        daily_data.columns = ['Coin','Timestamp','Transfer','From','To','Amount','unit','child','parent',
+                             'title','input','tags','format','block']
+        daily_data = daily_data[daily_data['Transfer']=='transfer']
+        ### We have problems with sheer size of data, so we take every opportunity to reduce the size.
+        del(daily_data['Coin'])
+        del(daily_data['Transfer'])
+        daily_data = daily_data.reset_index()
+        daily_data['Amount'] = pd.to_numeric(daily_data['Amount'], errors='ignore')
+        ### And then we iterate through functions. First we prepare arrays and basic computations.
+        array1 , dates_array, to_array, first_occurance = reputation_calc_p1(daily_data,first_occurance)
+        del(daily_data)
+        reputation = update_reputation(reputation,array1)
+        ### And then update reputation.
+        new_reputation = calculate_new_reputation(array1,to_array,reputation)
+        ### In our case we take approach c.
+        reputation = update_reputation_approach_c(first_occurance,reputation,new_reputation,since,our_date)
+        ### Finally we save file to disk.
+        our_file = os.path.join(os.getcwd(),"reputations","reputation_"+str(our_date)[0:10]+".data")
+        fp=gzip.open(our_file,'wb')
+        pickle.dump(reputation,fp)
+        fp.close()                
+                
+
+    print("Ending time is:",timeit.timeit()-start)
+    
+    ### Save the dictionary of first occurances as well...
+    our_file = os.path.join(os.getcwd(),"reputations","first_occurances.data")
+    fp=gzip.open(our_file,'wb')
+    pickle.dump(first_occurance,fp)
+    fp.close()             
+run_script()    
     
