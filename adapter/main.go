7934fdd6d9c9c3cbc91bdd3647aafd8101c6b632
Marco Argentieri <marco@vulpem.com>
2018-11-15 16:56:08 +0100

    ChannelOpen event

--- adapter/main.go
+++ adapter/main.go
@@ -6,32 +6,34 @@ import (
 	"fmt"
 	"log"
 	"math/big"
+	"strings"
 
 	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/tiero/reputation/adapter/resources/contracts/mpe"
 )
 
 //Network config struct
 type Network struct {
 	RPCEndpoint     string
 	DeployedAddress common.Address
+	startingBlock   int64
 }
 
 var networks = map[string]Network{
 	"kovan": Network{
 		"https://kovan.infura.io",
 		common.HexToAddress("0x385036D6cd8Cf6A8749d5Df7f716F0341E1c13B1"),
+		0,
 	},
 	"ropsten": Network{
 		"https://ropsten.infura.io",
 		common.HexToAddress("0xAF5e3b8CF89815F24A12D45D4758D87257249778"),
+		4429391,
 	},
 }
-
-const (
-	startingBlock int64 = 4429391
-)
 
 func main() {
 	networkKey := flag.String("network", "ropsten", "network. One of {mainnet, ropsten, kovan}")
@@ -43,7 +45,7 @@ func main() {
 	}
 
 	query := ethereum.FilterQuery{
-		FromBlock: big.NewInt(startingBlock),
+		FromBlock: big.NewInt(currentNetwork.startingBlock),
 		Addresses: []common.Address{currentNetwork.DeployedAddress},
 	}
 
@@ -52,8 +54,28 @@ func main() {
 		log.Fatal(err)
 	}
 
+	mpeAbi, err := abi.JSON(strings.NewReader(string(mpe.MpeABI)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	for _, vLog := range logs {
 		fmt.Println(vLog.TxHash.Hex())
+		ChannelOpenEvent := struct {
+			ChannelID  *big.Int
+			Sender     common.Address
+			Recipient  common.Address
+			GroupID    [32]byte
+			Signer     common.Address
+			Amount     *big.Int
+			Expiration *big.Int
+		}{}
+		err := mpeAbi.Unpack(&ChannelOpenEvent, "ChannelOpen", vLog.Data)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(ChannelOpenEvent.Amount)
 	}
 
 }
