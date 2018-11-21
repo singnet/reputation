48d991e537643b6f449aa88aaf2c7b5571e0fda0
nejc9921 <33785967+nejc9921@users.noreply.github.com>
2018-11-21 17:46:01 +0100

    Creating reputation for no-ratings
    Python code for reputation calculation when there are no rankings together with checking the correlation (reputation_calculation_statistics.py). It uses other files in directory to generate transactions and users.

---
+++ agency/python/src/reputation_calculation_statistics.py
@@ -0,0 +1,36 @@
+### Here we prepare the script for a "quick" run.
+from liquid_rank import * 
+import datetime
+### Default reputation (starting one) and conservativity.
+default_rep = 0.5
+conservativity = 0.5
+#Unhealthy agent environment set 
+#good_agent = {"range": [1,8], "values": [100,1000], "transactions": 10}
+#bad_agent = {"range": [9,10], "values": [10,100], "transactions": 100}
+
+#Semi-healthy agent environment set 
+#good_agent = {"range": [1,8], "values": [100,1000], "transactions": 10}
+#bad_agent = {"range": [9,10], "values": [5,50], "transactions": 100}
+days = 10
+multiplier = 1/days # Each day of reputation calculation adds this much weight to avg_reputation.
+
+#Healthy agent environment set (default) 
+good_agent = {"range": [1,8], "values": [100,1000], "transactions": 100}
+bad_agent = {"range": [9,10], "values": [1,10], "transactions": 100}
+
+# False - financial, True - ratings
+simulate(good_agent,bad_agent, datetime.date(2018, 10, 1), days, False)
+reputation, avg_reputation = calculate_reputations(default_rep,conservativity,multiplier)
+
+avg_reputations = []
+for i in range(1,11):
+    avg_reputations.append(avg_reputation[i])
+
+reputations = []
+for i in range(1,11):
+    reputations.append(reputation[i])
+    
+    
+real_values = [1,1,1,1,1,1,1,1,0,0] ### Actual values.
+print("latest correlation",np.corrcoef(reputations, real_values)[0][1] )
+print("average correlation",np.corrcoef(avg_reputations, real_values)[0][1] )
