da6ed78074e158ad89689f76e8dc4535dff70c60
nejc9921 <33785967+nejc9921@users.noreply.github.com>
2018-11-29 21:22:23 +0100

    Changes in computation, speeding it up and allowing for simulation of simulation of share of suppliers/consumers
    Changes in computation, speeding it up and allowing for simulation of simulation of share of suppliers/consumers

--- agency/python/src/reputation_calculation.py
+++ agency/python/src/reputation_calculation.py
@@ -22,13 +22,6 @@
 
 # Reputation Service API, including Rating Service and Ranking Service
 
-
-# reputation_scenario_test
-from reputation_scenario_test import pick_agent, log_file, simulate
-import random
-import datetime
-import time
-
 ### Import packages
 import csv
 import pandas as pd
@@ -39,7 +32,12 @@ from datetime import datetime, timedelta
 import pickle
 import gzip
 import time
-import timeit
+
+# reputation_scenario_test
+import random
+import datetime
+import time
+from aigents_api import *
 
 
 ### Get strings between two strings; will be useful when extracting the date.
@@ -68,8 +66,8 @@ def parse_prefix(line, fmt):
     return datetime.strptime(line[:cover], fmt)
 
 def days_between(d1, d2):
-    d1 = parse_prefix(d1,"%Y-%m-%d")
-    d2 = parse_prefix(d2,"%Y-%m-%d")
+    #d1 = parse_prefix(d1,"%Y-%m-%d")
+    #d2 = parse_prefix(d2,"%Y-%m-%d")
     return abs((d2 - d1).days)    
     
 ###   Get starting dates and first occurances of each addresses. Also, preparation or arrays and other data
@@ -85,13 +83,13 @@ def reputation_calc_p1(new_subset,first_occurance):
     ### The easiest (and in pandas probably the fastest) way would be to create sorted dataframe and then iterate
     ### and check if we already have certain record. If no, create, if yes, it was created before, so pass.
     ### When it is created we also store date of creation.
-    sorted_merge = new_subset.sort_values(['Timestamp'], ascending=[True])
+    sorted_merge = new_subset.sort_values(['Date'], ascending=[True])
     sorted_merge = sorted_merge.reset_index()
     ### Time to do some refinements. Let's get rid of Pandas dataframe and save it to something else.
     ### Let us sort the dataset alphabetically by "To". This can fasten up the algo later on...
 
     new_array = new_subset[['From','To','Amount','Rating']].values
-    dates_array = np.array(sorted_merge['Timestamp'])
+    dates_array = np.array(sorted_merge['Date'])
     to_array = np.array(new_subset['To'].values)
 
     
@@ -249,7 +247,22 @@ def calculate_new_reputation_no_log(new_array,to_array,reputation,rating,normali
         
 
     return(mys)
-
+### Initialize dictionary with all keys from our dataset and 0 values;
+def initialize_dict(from_array,to_array):
+    mydict = {}
+    for k in np.unique(from_array):
+        if k in mydict.keys():
+            pass
+        ## If we do not have this record, we set it default reputation.
+        else:
+            mydict[k] = 0
+    for k in np.unique(to_array):
+        if k in mydict.keys():
+            pass
+        ## If we do not have this record, we set it default reputation.
+        else:
+            mydict[k] = 0
+    return(mydict)
 
 
 
@@ -286,52 +299,174 @@ def avg_rep_calculate(avg_reputation,new_reputation,multiplier):
         else:
             pass
     return(avg_reputation)
+    
+#run_script()    
+network = 'reptest'
+def pick_agent(list,self,memories = None,bad_agents = None):
+	picked = None
+	if memories is not None:
+		if self in memories:
+			blacklist = memories[self]
+		else:
+			blacklist = []
+			memories[self] = blacklist
+	else:
+		blacklist = None
+	while(picked is None):
+		picked = list[random.randint(0,len(list)-1)]
+		if picked == self:
+			picked = None # skip self
+		else:
+			if blacklist is not None and picked in blacklist:
+				picked = None # skip those who already blacklisted
+	if blacklist is not None and bad_agents is not None and picked in bad_agents:
+		blacklist.append(picked) #blacklist picked bad ones once picked so do not pick them anymore
+	return picked
+
+
+def log_file(file,date,type,agent_from,agent_to,cost,rating):
+	timestr = str(time.mktime(date.timetuple()))
+	timestr = timestr[:-2] # trim .0 part
+	file.write(network + '\t' + timestr + '\t' + type + '\t' + str(agent_from) + '\t' + str(agent_to) + '\t' + str(cost) + '\t' \
+			+ '\t\t\t\t\t\t\t\t' + ('' if rating is None else str(rating)) + '\t\n')
+
+
+def simulate(good_agent,bad_agent,since,sim_days,ratings):
+	network='reptest'
+	random.seed(1) # Make it deterministic
+	memories = {} # init blacklists of compromised ones
+	
+	#print('Good:',good_agent)
+	#print('Bad:',bad_agent)
+	
+	good_agents = [i for i in range(good_agent['range'][0],good_agent['range'][1]+1)]
+	bad_agents = [i for i in range(bad_agent['range'][0],bad_agent['range'][1]+1)]
+	#print('Good:',good_agents)
+	#print('Bad:',bad_agents)
+	
+	all_agents = good_agents + bad_agents
+
+	#print('All:',all_agents)
+	
+	good_agents_transactions = good_agent['transactions']
+	bad_agents_transactions = bad_agent['transactions']
+	good_agents_values = good_agent['values']
+	bad_agents_values = bad_agent['values']
+	good_agents_count = len(good_agents)
+	bad_agents_count = len(bad_agents)
+	good_agents_volume = good_agents_count * good_agents_transactions * good_agents_values[0]
+	bad_agents_volume = bad_agents_count * bad_agents_transactions * bad_agents_values[0]
+	code = ('r' if ratings else 'p') + '_' + str(round(good_agents_values[0]/bad_agents_values[0])) + '_' + str(good_agents_transactions/bad_agents_transactions) 
+	transactions = 'transactions_' + code + '.tsv'
+	transactions="transactions.tsv"
+	print('Good:',len(good_agents),good_agents_values[0],good_agents_transactions,len(good_agents)*good_agents_values[0]*good_agents_transactions)
+	print('Bad:',len(bad_agents),bad_agents_values[0],bad_agents_transactions,len(bad_agents)*bad_agents_values[0]*bad_agents_transactions)
+	print('Code:',code,'Volume ratio:',str(good_agents_volume/bad_agents_volume))
+    
+    
+    ### Get transactions in;
+	network = []
+	Timestamp = []
+	type1 = []
+	From = []
+	To = []
+	Rating = []
+	Amount = []    
+	with open(transactions, 'w') as file:
+		for day in range(sim_days):
+			date = since + datetime.timedelta(days=day)
+			#print(day,date,memories)
+
+			for agent in good_agents:
+				for t in range(0, good_agents_transactions):
+					other = pick_agent(all_agents,agent,memories,bad_agents)
+					cost = random.randint(good_agents_values[0],good_agents_values[1])
+					#i = len(mynewdf)
+					#mynewdf.loc[i] = 0
+					if ratings:
+						# while ratings range is [0.0, 0.25, 0.5, 0.75, 1.0], we rank good agents as [0.25, 0.5, 0.75, 1.0]
+						rating = 0.0 if other in bad_agents else float(random.randint(1,4))/4
+						log_file(file,date,'rating',agent,other,rating,cost)
+						network.append('reptest')
+						Timestamp.append(date)
+						type1.append('rating')
+						From.append(agent)
+						To.append(other)
+						Rating.append(rating)
+						Amount.append(cost)
+
+					else: 
+						network.append('reptest')
+						Timestamp.append(date)
+						type1.append('rating')
+						From.append(agent)
+						To.append(other)
+						Rating.append(np.nan)
+						Amount.append(cost)                        
+						log_file(file,date,'transfer',agent,other,cost,None)
+			for agent in bad_agents:
+				for t in range(0, bad_agents_transactions):
+					#i = len(mynewdf)
+					#mynewdf.loc[i] = 0
+					other = pick_agent(bad_agents,agent)
+					cost = random.randint(bad_agents_values[0],bad_agents_values[1])
+					if ratings:
+						rating = 1.0
+						log_file(file,date,'rating',agent,other,rating,cost)
+						network.append('reptest')
+						Timestamp.append(date)
+						type1.append('rating')
+						From.append(agent)
+						To.append(other)
+						Rating.append(rating)
+						Amount.append(cost)                                          
+					else: 
+						log_file(file,date,'transfer',agent,other,cost,None)
+						network.append('reptest') #adsa
+						Timestamp.append(date)
+						type1.append('rating')
+						From.append(agent)
+						To.append(other)
+						Rating.append(np.nan)
+						Amount.append(cost)                       
+	with open('users.tsv', 'w') as file:
+		for agent in all_agents:
+			goodness = '0' if agent in bad_agents else '1'
+			file.write(str(agent) + '\t' + goodness + '\n')
+	print(len(network))   
+	dfs = {'network':network,'Date':Timestamp,'type':type1,'From':From,'To':To,
+          'Rating':Rating,'Amount':Amount}
+	mynewdf = pd.DataFrame(data=dfs)
+	return(mynewdf)
 
 
 
-
-def calculate_reputations(default_rep,conservativity,multiplier,ratings):
+def calculate_reputations(data,default_rep,conservativity,multiplier,ratings):
     from datetime import datetime, timedelta
     ### Define empty dictionaries where we save our queries later on.
     first_occurance = dict()
     reputation = dict()
     previous_reputation = dict()
-    folder=""
-    start = timeit.timeit()
+    start = time.time()
     ### We read from transactions.tsv.
-    filename = 'transactions.tsv'
-    filepath = os.path.join(folder, filename)
-    print(filepath)
+
     daily_data = []
 
-    data = pd.read_csv(filepath,delimiter="\t",header=None)
-    ### Here we name the columns. This is important and might be changed as specifications change...
-    if ratings:
-        data.columns = ['network','Timestamp','type','From','To','Rating','unit','child','parent','title','input','tags','format',
-                     'block','Amount','parent_unit']
-    else:
-        data.columns = ['network','Timestamp','type','From','To','Amount','unit','child','parent','title','input','tags','format',
-                     'block','Rating','parent_unit']
-    ### Simple renaming is usually needed.
-    data = data.rename(columns={"from":"From","to":"To"})
-    data['Date'] = " "
-    i = 0
-    while i<len(data):
-
-        data['Date'][i] = datetime.utcfromtimestamp(data['Timestamp'][i]).strftime('%Y-%m-%d')
-        i+=1
     data = data.sort_values(['Date'], ascending=[True])   
+    #data['Date'] = mydate
     ### And now we can calculate days since start, so we can iterate through dates.
     data['days_since_start'] = " "
+    mydate = []
     i = 0
     while i<len(data):
-        data['days_since_start'][i] = days_between(data['Date'][i],data['Date'][0])
+        mydate.append(days_between(data['Date'][i],data['Date'][0]))
 
         i+=1
-    get_arrays , dates_array, to_array, first_occurance = reputation_calc_p1(data,first_occurance)
-    avg_reputation = update_reputation({},get_arrays,0)
+    data['days_since_start'] = mydate
+    avg_reputation = initialize_dict(data['From'],data['To'])
     ### We will also calculate average reputation. We had to sort the whole dataset at the beginning, so that we have
     ### enough 
+
     i= 0
     while i<len(np.unique(data['days_since_start'])):
         mysubset = data[data['days_since_start']==i]
@@ -341,7 +476,8 @@ def calculate_reputations(default_rep,conservativity,multiplier,ratings):
         daily_data = daily_data.rename(columns={"from":"From","to":"To"})
         mydate = mysubset['Date'].loc[0]
         
-        our_date = datetime.strptime(mydate, '%Y-%m-%d')
+        #our_date = datetime.strptime(mydate, '%Y-%m-%d')
+        our_date = mydate
         since = our_date - timedelta(days=1)
 
         ### And then we iterate through functions. First we prepare arrays and basic computations.
@@ -349,9 +485,7 @@ def calculate_reputations(default_rep,conservativity,multiplier,ratings):
         del(daily_data)    
         reputation = update_reputation(reputation,array1,default_rep)
         ### And then update reputation.
-        new_reputation = calculate_new_reputation_no_log(new_array = array1,
-                                                                 to_array = to_array,reputation = reputation,
-                                                                 rating = ratings)
+        new_reputation = calculate_new_reputation_no_log(array1,to_array,reputation,ratings)
         ### In our case we take approach c.
         reputation = update_reputation_approach_d(first_occurance,reputation,new_reputation,since,our_date,
                                                  default_rep,conservativity)
@@ -360,19 +494,10 @@ def calculate_reputations(default_rep,conservativity,multiplier,ratings):
         avg_reputation = avg_rep_calculate(avg_reputation,reputation,multiplier)
 
         save_zipped_pickle(reputation,our_file)
-
+        #print("Day",i,"completed.")
         i+=1
-    print("Ending time is:",timeit.timeit()-start)
+    print("Ending time is:",time.time()-start)
     return(reputation,avg_reputation)
 
 
-import datetime
-
     
-
-
-
-
-
-
-
