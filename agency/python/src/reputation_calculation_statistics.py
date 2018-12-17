bcc4f93189d78e4de78ead5d64d0667fbf491b42
nejc9921 <33785967+nejc9921@users.noreply.github.com>
2018-12-17 22:08:16 +0100

    reputation service API
    reputation service API

--- agency/python/src/reputation_calculation_statistics.py
+++ agency/python/src/reputation_calculation_statistics.py
@@ -21,41 +21,91 @@
 # SOFTWARE.
 
 # Reputation Service API, including Rating Service and Ranking Service
+from reputation_service_api import *
 
-from reputation_calculation import *
-### Here we prepare the script for a "quick" run.
-default_rep = 0.1
-conservativity = 0.5
-#Unhealthy agent environment set 
-#good_agent = {"range": [1,8], "values": [100,1000], "transactions": 10}
-#bad_agent = {"range": [9,10], "values": [10,100], "transactions": 100}
-
-#Semi-healthy agent environment set 
-#good_agent = {"range": [1,8], "values": [100,1000], "transactions": 10}
-#bad_agent = {"range": [9,10], "values": [5,50], "transactions": 100}
+### First define what we want;
+### We measure time spent:
+start = time.time()
+## Define our reputation_system class
+reputation_system = PythonReputationService()
+### Set parameters!
+reputation_system.set_parameters(default=0.5,conservaticism=0.5,precision=0.01,weighting=True,fullnorm= True,
+                      liquid=False,logranks=False,temporal_aggregation=False,logratings=False,days_jump=1,
+                      use_ratings = True,start_date=datetime.date(2018, 1, 1))
+### Further setting paramters; number of days and multiplier day/nr of days.
 days = 180
 multiplier = 1/days # Each day of reputation calculation adds this much weight to avg_reputation.
+# Generate simulated marketplace and save it to data.
+good_agent = {"range": [1,800], "values": [100,1000], "transactions": 10, "suppliers": 0.5, "consumers": 0.5}
+bad_agent = {"range": [801,1000], "values": [1,10], "transactions": 100, "suppliers": 0.5, "consumers": 0.5}
+data = simulate(good_agent,bad_agent, reputation_system.date, days,reputation_system.use_ratings)
+### We read from transactions.tsv or just continue with data object. Further improvements of speed are possible
+### if we decide not to write transactions.tsv to disk - this is slowing down the algorithm.
+daily_data = []
+data = data.sort_values(['Date'], ascending=[True])   
+### And now we can calculate days since start, so we can iterate through dates.
+data['days_since_start'] = " "
+mydate = []
+### We sort data by date. So far all in pandas. Also, below loop is not necessary, but it will define
+### in which day a certain observation is.
+i = 0
+while i<len(data):
+    mydate.append(days_between(data['Date'][i],data['Date'][0]))
+    i+=1
+data['days_since_start'] = mydate
+### Computing average reputation is not part of reputation system, so we have to do it ourselves.
+avg_reputation = initialize_dict(data['From'],data['To'])
+### We will also calculate average reputation. We had to sort the whole dataset at the beginning, so that we have
+### enough 
+### We start everything with initializing ranks - putting them to default rank.
+reputation_system.initialize_ranks()
+i= 0
+while i<len(np.unique(data['days_since_start'])):
+    ### We loop through dates and take a subset of each date.
+    mysubset = data[data['Date']==reputation_system.date]
+    mysubset = mysubset.reset_index()
+    daily_data = mysubset
+    ### Columns need to be renamed because my previous version is non-standardised.
+    daily_data = daily_data.rename(columns={"from":"From","to":"To"})
+    mydate = mysubset['Date'].loc[0]
+    ### we change the date in our reputation system class.
+    reputation_system.set_date(mydate)
+    since = reputation_system.our_date - timedelta(days=reputation_system.days_jump)
 
-#Healthy agent environment set (default) 
-good_agent = {"range": [1,800],  "values": [100,1000], "transactions": 10, "suppliers": 0.5, "consumers": 0.5}
-bad_agent = {"range": [801,1000], "values": [10,100], "transactions": 100, "suppliers": 0.5, "consumers": 0.5}
-ratings = True
-temporal_aggregation = True
-# False - financial, True - ratings
-mysim = simulate(good_agent,bad_agent, datetime.date(2018, 1, 1), days,ratings)
-### Inputs: default reputation (recommended 0.1 or 0.5), conservativity (recommended 0.5),multiplier (depends
-### on nr of days), ratings True/False - so do we include them?
-reputation, avg_reputation = calculate_reputations(mysim,default_rep,conservativity,multiplier,ratings,temporal_aggregation)
+    ### And update the ratings. This just adds mysubset, so a subset in our analysis in this particular moment.
+    reputation_system.update_ratings(daily_data)
+    ### After that, we update ranks.
+    reputation_system.update_ranks()
+    del(daily_data)   
+    ### Finally we save file to disk. Not necessary and part of reputation service, but it's within this loop.
+    our_file = os.path.join(os.getcwd(),"reputations","reputation_"+str(reputation_system.our_date)[0:10]+".data")
+    avg_reputation = avg_rep_calculate(avg_reputation,reputation_system.reputation,multiplier)
+    ### Save the reputation data in /reputations map.
+    save_zipped_pickle(reputation_system.reputation,our_file)
+    reputation_system.add_time() ### Go to next day
+    print("Day",i,"completed.")
+    i+=1
+print("Ending time is:",time.time()-start)
 
+### Now reputation computation is finished and we just calculate correlations.
 avg_reputations = []
-for i in range(1,11):
+for i in range(1,len(reputation_system.reputation)):
     avg_reputations.append(avg_reputation[i])
-
+    
 reputations = []
-for i in range(1,11):
-    reputations.append(reputation[i])
+for i in range(1,len(reputation_system.reputation)):
+    reputations.append(reputation_system.reputation[i])    
+real_values = []
+i = 1
+while i<(len(reputation_system.reputation)):
+    if i<0.80001 * (len(reputation_system.reputation)):
+        real_values.append(1)
+    else:
+        real_values.append(0)
+    i +=1  
     
     
-real_values = [1,1,1,1,1,1,1,1,0,0] ### Actual values.
-print("latest correlation",np.corrcoef(reputations, real_values)[0][1] )
-print("average correlation",np.corrcoef(avg_reputations, real_values)[0][1] )
+### average
+print("average Pearson:",np.corrcoef(avg_reputations, real_values)[0][1])
+### Latest
+print("last Pearson:",np.corrcoef(reputations, real_values)[0][1])
--- agency/python/src/reputation_service_api.py
+++ agency/python/src/reputation_service_api.py
@@ -23,52 +23,107 @@
 # Reputation Service API, including Rating Service and Ranking Service
 
 import abc
-from reputation_api import *
+from reputation_service_api import *
+from reputation_calculation import *
 
 #TODO @neic to implement
-# test
+
 """
 Reputation Service native implementation in Python
 """        
-class PythonReputationService(RatingService,RankingService):
-
-	def clear_ratings(self):
-		return("clear_ratings")
-
-	def put_ratings(self,ratings):
-		return("put_ratings")
-
-	def get_ratings(self,filter):
-		return("get_ratings")
-
-	def clear_ranks(self):
-		return("clear_ranks")
-
-	def put_ranks(self,date,ranks):
-		return("put_ranks")
-
-	def get_ranks(self,filter):
-		return("get_ranks")
-
-	def update_ranks(self,date):
-		return("update_ranks")
-
-	def set_parameters(self,parameters):
-		return("set_parameters")
-
-	def get_parameters(self):
-		return("get_parameters")
 
 
-#TODO @nejc move this out to unittest eventually
-import datetime
-rs = PythonReputationService()
-print( rs.clear_ratings() )
-print( rs.clear_ranks() )
-print( rs.put_ratings([]) )
-print( rs.put_ranks(datetime.date(2018, 1, 1),{}) )
-print( rs.get_ratings({}) )
-print( rs.get_ranks({}) )
-print( rs.update_ranks(datetime.date(2018, 1, 1)) )
-print( rs.set_parameters({"param1":"value1","param2":"value2"}) )
-print( rs.get_parameters() )
+
+##TODO @nejc move this out to unittest eventually
+#import datetime
+#rs = PythonReputationService()
+#print( rs.clear_ratings() )
+#print( rs.clear_ranks() )
+#print( rs.put_ratings([]) )
+#print( rs.put_ranks(datetime.date(2018, 1, 1),{}) )
+#print( rs.get_ratings({}) )
+#print( rs.get_ranks({}) )
+#print( rs.update_ranks(datetime.date(2018, 1, 1)) )
+#print( rs.set_parameters({"param1":"value1","param2":"value2"}) )
+#print( rs.get_parameters() )
+
+
+class PythonReputationService(object):
+    
+    ### This function allows us to set up the parameters.
+    ### Setting up the way we do in Anton's recommendation.
+    ### days_jump is how many days we jump in one period... We can adjust it...
+    def set_parameters(self,default=0.5,conservaticism=0.5,precision=0.01,weighting=True,fullnorm= True,
+                      liquid=False,logranks=True,temporal_aggregation=False,logratings=False,days_jump=1,
+                      use_ratings = True,start_date=datetime.date(2018, 1, 1)):
+        self.default=default
+        self.conservaticism = conservaticism
+        self.precision = precision
+        self.weighting = weighting
+        self.fullnorm = fullnorm
+        self.liquid=liquid
+        self.logranks = logranks
+        self.temporal_aggregation = temporal_aggregation
+        self.logratings = logratings
+        self.days_jump = days_jump
+        self.use_ratings = use_ratings      
+        self.date = start_date
+        
+    ### This functions merely displays the parameters.
+    def get_parameters(self):
+        print("default=",self.default,",conservatism=",self.conservaticism,",precision=",self.precision,
+             ",weighting=",self.weighting,",fullnorm=",self.fullnorm,",liquid=",self.liquid,",logranks=",self.logranks,
+             ",temporal_aggregation=",self.temporal_aggregation,",logratings=",self.logratings,",days_jump=",self.days_jump,
+             "use_ratings=",self.use_ratings,"date=",self.date)
+    ## Update date
+    def set_date(self,newdate):
+        self.our_date = newdate
+    def clear_ranks(self):
+        self.ranks = {}    
+        
+    def initialize_ranks(self,reputation=None,first_occurance = None):
+        if reputation==None:
+            self.reputation = {}
+        else:
+            self.reputation = reputation
+        if first_occurance==None:
+            self.first_occurance = {}
+        else:
+            self.first_occurance = first_occurance            
+      
+    def update_ranks(self):
+        ### And then we iterate through functions. First we prepare arrays and basic computations.
+        start1 = time.time()
+        
+        array1 , dates_array, to_array, first_occurance = reputation_calc_p1(self.ratings,self.first_occurance,
+                                                                             self.temporal_aggregation)  
+        self.first_occurance = first_occurance
+        self.reputation = update_reputation(self.reputation,array1,self.default)
+        since = self.our_date - timedelta(days=self.days_jump)
+        ### And then update reputation.
+        if self.logranks:
+            new_reputation = calculate_new_reputation(array1,to_array,self.reputation,self.use_ratings,
+                                                      normalizedRanks=self.fullnorm,weighting = self.weighting,
+                                                     liquid = self.liquid, logratings = self.logratings)
+        else:
+            new_reputation = calculate_new_reputation_no_log(array1,to_array,self.reputation,self.use_ratings,
+                                                            normalizedRanks=self.fullnorm,weighting = self.weighting,
+                                                     liquid = self.liquid, logratings = self.logratings)
+        ### In our case we take approach c.
+        self.reputation = update_reputation_approach_d(self.first_occurance,self.reputation,new_reputation,since,
+                                                       self.our_date, self.default,self.conservaticism)
+        
+    def update_ratings(self, ratings):
+        if ratings is None:
+            print("No data detected")
+        else:
+            self.ratings = ratings
+    
+    def get_ranks(self):
+        print(self.reputation)
+    def add_time(self,addition=0):
+        if addition==0:
+            self.date = self.date + timedelta(days=self.days_jump)
+        else:
+            self.date = self.date + timedelta(days=addition)    
+
