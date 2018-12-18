280c8eced9d1ec024afef3958c9228ae51de284e
Anton Kolonin <akolonin@gmail.com>
2018-12-18 16:17:23 +0700

    Separate reputation unittest files

--- agency/python/src/test_reputation_simulation.py
+++ agency/python/src/test_reputation_simulation.py
@@ -20,109 +20,16 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# Reputation Service API Integration Testing
+# Test Reputation Batch Simulation
 
 import unittest
 import datetime
 import time
 import logging
+import subprocess
+import os
 
-from aigents_reputation_cli import *
 from aigents_reputation_api import *
-
-# Uncomment this for logging to console
-#logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-
-class TestReputationServiceBase(object):
-
-	def test_smoke(self):
-		rs = self.rs
-		
-		#check default parameters
-		p = rs.get_parameters()
-		self.assertEqual( p['default'], 0.5 )
-		self.assertEqual( p['conservatism'], 0.5)
-		self.assertEqual( p['precision'], 0.01)
-		self.assertEqual( p['weighting'], True)
-		self.assertEqual( p['fullnorm'], True)
-		self.assertEqual( p['liquid'], True)
-		self.assertEqual( p['logranks'], True)
-		self.assertEqual( p['aggregation'], False)
-		
-		#check default parameters
-		self.assertEqual( rs.set_parameters({'precision':0.011,'fullnorm':False}), 0 )
-		p = rs.get_parameters()
-		self.assertEqual( p['precision'], 0.011)
-		self.assertEqual( p['fullnorm'], False)
-		self.assertEqual( rs.set_parameters({'precision':0.01,'fullnorm':True}), 0 )
-		
-		#clear everything
-		self.assertEqual( rs.clear_ratings(), 0 )
-		self.assertEqual( rs.clear_ranks(), 0 )
-
-		dt1 = datetime.date(2018, 1, 1)
-		dt2 = datetime.date(2018, 1, 2)
-
-		#make sure that we have neither ranks not ratings
-		result, ranks = rs.get_ranks({'date':dt1})
-		self.assertEqual(result, 0)
-		self.assertEqual(len(ranks), 0)
-
-		filter = {'ids':[4],'since':dt2,'until':dt2} 
-		result, ratings = rs.get_ratings(filter)
-		self.assertEqual(result, 0)
-		self.assertEqual(len(ratings), 0)
-
-		#add ranks and make sure they are added
-		self.assertEqual( rs.put_ranks(dt1,[{'id':1,'rank':50},{'id':2,'rank':50},{'id':3,'rank':50}]), 0 )
-		result, ranks = rs.get_ranks({'date':dt1})
-		self.assertEqual(result, 0)
-		self.assertEqual(len(ranks), 3)
-
-		#add ratings and make sure they are added
-		ratings = [\
-			{'from':1,'type':'rating','to':3,'value':100,'weight':None,'time':dt2},\
-			{'from':1,'type':'rating','to':4,'value':100,'weight':None,'time':dt2},\
-			{'from':2,'type':'rating','to':4,'value':100,'weight':None,'time':dt2},\
-			{'from':4,'type':'rating','to':5,'value':100,'weight':None,'time':dt2},\
-			]
-		self.assertEqual( rs.put_ratings(ratings), 0 )
-		
-		#TODO end up with output format of the get_ratings method and extend unit test to check the data after then
-		#now we are just counting number of ratings returned in free format 
-		result, ratings = rs.get_ratings(filter)
-		self.assertEqual(result, 0)
-		self.assertEqual(len(ratings), 3)
-		ratings = sorted(ratings, key=lambda elem: "%s %s" % (elem['from'], elem['to']))
-		self.assertEqual(ratings[0]['from'], '4')
-		self.assertEqual(ratings[0]['to'], '1')
-		self.assertEqual(ratings[1]['to'], '2')
-		self.assertEqual(ratings[1]['value'], 100.0)
-		self.assertEqual(ratings[2]['time'], dt2)
-		
-		#TODO test for filter with
-		# multiple id-s
-		# from
-		# to
-		#update and get ranks
-		result, ranks = rs.get_ranks({'date':dt2})
-		self.assertEqual(result, 0)
-		self.assertEqual(len(ranks), 0)
-
-		self.assertEqual(rs.update_ranks(dt2), 0)
-
-		result, ranks = rs.get_ranks({'date':dt2})
-		self.assertEqual(result, 0)
-		self.assertEqual(len(ranks), 5)
-		
-		for rank in ranks:
-			if rank['id'] == '4':
-				self.assertEqual(rank['rank'], 100)
-			if rank['id'] == '1':
-				self.assertEqual(rank['rank'], 33)			
-
-"""
-# Test Reputation Batch Simulation
 from reputation_scenario import reputation_simulate 
 
 class TestReputationSimulation(unittest.TestCase):
@@ -189,4 +96,3 @@ class TestReputationSimulation(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-"""
