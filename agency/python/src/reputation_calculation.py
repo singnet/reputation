f4143eeb27675c9a1d83820b8ac3be27966fb747
nejc9921 <33785967+nejc9921@users.noreply.github.com>
2019-01-09 22:20:46 +0100

    Reputation test
    Reputation test implementation in Python.  Not finished yet.

--- agency/python/src/reputation_calculation.py
+++ agency/python/src/reputation_calculation.py
@@ -183,9 +183,6 @@ def simulate(good_agent,bad_agent,since,sim_days,ratings):
 
 
 
-
-
-
 ### Get strings between two strings; will be useful when extracting the date.
 def find_between( s, first, last ):
     try:
@@ -204,7 +201,6 @@ def find_between_r( s, first, last ):
     except ValueError:
         return ""
 
-    
 ### Below two functions will make sure we get difference between times.
 def parse_prefix(line, fmt):
     from datetime import datetime, timedelta
@@ -212,8 +208,6 @@ def parse_prefix(line, fmt):
     return datetime.strptime(line[:cover], fmt)
 
 def days_between(d1, d2):
-    #d1 = parse_prefix(d1,"%Y-%m-%d")
-    #d2 = parse_prefix(d2,"%Y-%m-%d")
     return abs((d2 - d1).days)    
     
 ###   Get starting dates and first occurances of each addresses. Also, preparation or arrays and other data
@@ -221,34 +215,35 @@ def days_between(d1, d2):
 ### Note; Given that we take an approach where we don't need first_occurance, we decide to put as a default option
 ### need_occurance=False.
 def reputation_calc_p1(new_subset,first_occurance,temporal_aggregation=False,need_occurance=False):
-    ### We first sort and delete level_0 column, if exists (not needed)
-    new_subset = new_subset.sort_values(['To'], ascending=[True])
-     
-    if 'level_0' in new_subset:
-        del(new_subset['level_0'])
-    new_subset = new_subset.reset_index() 
     #### We will need from, to, amount, the rest is not necessary to have - let's save memory.
     ### Now we will just store the first occurance of each account in a dictionary (first_occurance)
     ### The easiest (and in pandas probably the fastest) way would be to create sorted dataframe and then iterate
     ### and check if we already have certain record. If no, create, if yes, it was created before, so pass.
     ### When it is created we also store date of creation.
-    sorted_merge = new_subset.sort_values(['Date'], ascending=[True])
-    if 'level_0' in sorted_merge:
-        del(sorted_merge['level_0'])  
-    sorted_merge = sorted_merge.reset_index()
-    
-    ### Time to do some refinements. Let's get rid of Pandas dataframe and save it to something else.
-    ### Let us sort the dataset alphabetically by "To". This can fasten up the algo later on...
 
-    new_array = new_subset[['From','To','Amount','Rating']].values
-    dates_array = np.array(sorted_merge['Date'])
-    to_array = np.array(new_subset['To'].values)
+    i=0
+    new_array = []
+    while i<len(new_subset):
+        if 'rating' in list(new_subset[i].keys()):
+            new_array.append([new_subset[i]['from'],new_subset[i]['to'],new_subset[i]['value'],new_subset[i]['rating']])
+        else:
+            new_array.append([new_subset[i]['from'],new_subset[i]['to'],new_subset[i]['value']])
+        i+=1
+
+    dates_array = []
+    to_array = []
+    i = 0
+    while i<len(new_subset):
+        dates_array.append(new_subset[i]['time'])
+        to_array.append(new_subset[i]['to'])
+        i+=1
     
+  
     uniques = []### This can probably be done better, but it's low priority ATM. The reason is that 
     ### we are not really using first_occurance in initial design.
     if need_occurance:
         i = 0
-        while i<len(sorted_merge):
+        while i<len(new_subset):
             if new_array[i][0] in uniques:
                 pass
             else:
@@ -329,7 +324,6 @@ def reputation_calc_p1(new_subset,first_occurance,temporal_aggregation=False,nee
                
         new_array = new_array2
         to_array = to_array2
-    del(sorted_merge)
     return(new_array,dates_array,to_array,first_occurance)
 ### Get new reputations in case we do not yet have the old ones.
 def update_reputation(reputation,new_array,default_reputation):
@@ -345,6 +339,7 @@ def update_reputation(reputation,new_array,default_reputation):
         if new_array[i][1] in reputation:
             pass
         else:
+            
             reputation[new_array[i][1]] = default_reputation
         i+=1
     return(reputation)
@@ -363,15 +358,14 @@ def calculate_new_reputation(new_array,to_array,reputation,rating,normalizedRank
             mys[new_array[i][1]] = 0
         i+=1
 
-    ### Problem!!! All results are the same!!
     unique_ids = np.unique(to_array)
     k=0
     i = 0
     if rating:
         while i<len(unique_ids):
             amounts = []
-            ### Here we get log transformation of each amount value.            
-            get_subset = np.where(to_array==unique_ids[i])[0]
+            ### Here we get log transformation of each amount value. 
+            get_subset = where(to_array,unique_ids[i])            
             k=0 
             for k in get_subset:
                
@@ -379,29 +373,28 @@ def calculate_new_reputation(new_array,to_array,reputation,rating,normalizedRank
                     if logratings:
                         amounts.append(np.log10(1+new_array[k][3]) * np.log10(1+new_array[k][2])* rater_reputation(reputation,new_array[k][0],liquid=liquid))
                     else:
+                        
                         amounts.append(new_array[k][3] * np.log10(1+new_array[k][2])* rater_reputation(reputation,new_array[k][0],liquid=liquid))
                 else:
                     if logratings:
                         amounts.append(np.log10(1+new_array[k][3]) * rater_reputation(reputation,new_array[k][0],liquid=liquid))
                     else:
                         amounts.append(new_array[k][3] * rater_reputation(reputation,new_array[k][0],liquid=liquid))
-                    ###new_array[k][2] is Amount, new_array[k][3] is rating
-                #if k==len(to_array)-1:
-                #    break
-                
+            
             mys[unique_ids[i]] = sum(amounts)
 
             i+=1
     else:
         while i<len(unique_ids):
-       
+        
             amounts = []
             ### Here we get log transformation of each amount value.            
-            get_subset = np.where(to_array==unique_ids[i])[0]
+            get_subset = where(to_array,unique_ids[i]) 
             k=0 
             for k in get_subset:
                 if weighting:
                     amounts.append(np.log10(1+new_array[k][2])* rater_reputation(reputation,new_array[k][0],liquid=liquid))
+                    
                 else:
                     amounts.append(rater_reputation(reputation,new_array[k][0],liquid=liquid))###new_array[k][2] is Amount, new_array[k][3] is rating
                 if k==len(to_array)-1:
@@ -428,8 +421,6 @@ def calculate_new_reputation(new_array,to_array,reputation,rating,normalizedRank
         else:
             mys[k] = mys[k] /max_value
     return(mys)
-
-
 ### Get updated reputations, new calculations of them...
 ### This one is with log...
 
@@ -439,7 +430,6 @@ def rater_reputation(previous_reputations,rater_id,liquid=False):
     else:
         rater_rep = previous_reputations[rater_id]
     return(rater_rep)
-
 
 def calculate_new_reputation_no_log(new_array,to_array,reputation,rating,normalizedRanks=True,weighting=True,
                                    liquid = True,logratings=False):
@@ -457,46 +447,34 @@ def calculate_new_reputation_no_log(new_array,to_array,reputation,rating,normali
     i = 0
     if rating:
         while i<len(unique_ids):
-            #print("len",len(to_array),"where:",np.where(to_array==unique_ids[i]),"and id",unique_ids[i])
             amounts = []
-            get_subset = np.where(to_array==unique_ids[i])[0]
+            get_subset = where(to_array,unique_ids[i])
             k=0 
             for k in get_subset:
-                #print(get_subset,"hmm",k)
-                #if unique_ids[i]==to_array[k]:
                 if weighting:
                     if logratings:
                         amounts.append(np.log10(1+new_array[k][3]) * new_array[k][2]* rater_reputation(reputation,new_array[k][0],liquid=liquid))
                     else:
                         amounts.append(new_array[k][3] * new_array[k][2]* rater_reputation(reputation,new_array[k][0],liquid=liquid))
-
                 else:
                     if logratings:
                         amounts.append(np.log(new_array[k][3]) * rater_reputation(reputation,new_array[k][0],liquid=liquid))
                     else:
                         amounts.append(new_array[k][3] * rater_reputation(reputation,new_array[k][0],liquid=liquid))
-
             mys[unique_ids[i]] = sum(amounts)
-
             i+=1
     else:
-
         while i<len(unique_ids):
-       
             amounts = []
             get_subset = np.where(to_array==unique_ids[i])[0]
             k=0 
             for k in get_subset:
-            
                     if weighting:
                         amounts.append(new_array[k][2]* rater_reputation(reputation,new_array[k][0],liquid=liquid))
                     else:
                         amounts.append(rater_reputation(reputation,new_array[k][0],liquid=liquid))
-
             mys[unique_ids[i]] = sum(amounts)
-            
-            i+=1        
-
+            i+=1
     ### nr 5.
     for k in mys.keys():
         if mys[k]<0:
@@ -512,7 +490,6 @@ def calculate_new_reputation_no_log(new_array,to_array,reputation,rating,normali
             mys[k] = (mys[k]-min_value) /(max_value-min_value)
         else:
             mys[k] = mys[k] /max_value
-
     return(mys)
 ### Initialize dictionary with all keys from our dataset and 0 values;
 def initialize_dict(from_array,to_array):
@@ -522,24 +499,21 @@ def initialize_dict(from_array,to_array):
             pass
         ## If we do not have this record, we set it default reputation.
         else:
-            mydict[k] = 0
+            mydict[str(k)] = 0
     for k in np.unique(to_array):
         if k in mydict.keys():
             pass
         ## If we do not have this record, we set it default reputation.
         else:
-            mydict[k] = 0
+            mydict[str(k)] = 0
     return(mydict)
-
-
 
 def update_reputation_approach_d(first_occurance,reputation,mys,since,our_date,default_rep,conservativity):
     ### Our current approach of updating reputation each time period. 
     j = 0
-
     all_keys = set(mys.keys())
     for k  in reputation.keys():
-        if k in all_keys:    
+        if k in all_keys:
             reputation[k] = (1-conservativity) * mys[k] + conservativity * reputation[k]
         else:
             reputation[k] = (1-conservativity) * default_rep + conservativity * reputation[k]
@@ -547,7 +521,14 @@ def update_reputation_approach_d(first_occurance,reputation,mys,since,our_date,d
         j+=1        
     return(reputation)
 
-
+def where(to_array,the_id):
+    our_ids = []
+    i=0
+    while i<len(to_array):
+        if to_array[i]==the_id:
+            our_ids.append(i)
+        i+=1
+    return(our_ids)
 
 def save_zipped_pickle(obj, filename, protocol=-1):
     with gzip.open(filename, 'wb') as f:
@@ -566,3 +547,4 @@ def avg_rep_calculate(avg_reputation,new_reputation,multiplier):
             pass
     return(avg_reputation)
     
+
--- agency/python/src/reputation_service_api.py
+++ agency/python/src/reputation_service_api.py
@@ -48,15 +48,67 @@ Reputation Service native implementation in Python
 #print( rs.get_parameters() )
 
 
+
+
 class PythonReputationService(object):
-    
     ### This function allows us to set up the parameters.
     ### Setting up the way we do in Anton's recommendation.
     ### days_jump is how many days we jump in one period... We can adjust it...
-    def set_parameters(self,default=0.5,conservaticism=0.5,precision=0.01,weighting=True,fullnorm= True,
-                      liquid=False,logranks=True,temporal_aggregation=False,logratings=False,days_jump=1,
-                      use_ratings = True,start_date=datetime.date(2018, 1, 1)):
-        self.default=default
+
+    def set_parameters(self,changes):
+        if 'default' in changes.keys():
+            default1 = changes['default']
+        else:
+            default1 = self.default
+        if 'conservaticism' in changes.keys():
+            conservaticism = changes['conservaticism']
+        else:
+            conservaticism = self.conservaticism
+        if 'precision' in changes.keys():
+            precision = changes['precision']
+        else:
+            precision = self.precision
+        if 'weighting' in changes.keys():
+            weighting = changes['weighting']
+        else:
+            weighting = self.weighting
+        if 'fullnorm' in changes.keys():
+            fullnorm = changes['fullnorm']
+        else:
+            fullnorm = self.fullnorm
+        if 'liquid' in changes.keys():
+            liquid = changes['liquid']
+        else:
+            liquid = self.liquid
+        if 'logranks' in changes.keys():
+            logranks = changes['logranks']
+        else:
+            logranks = self.logranks
+        if 'temporal_aggregation' in changes.keys():
+            temporal_aggregation = changes['temporal_aggregation']
+        else:
+            temporal_aggregation = self.temporal_aggregation
+        if 'logratings' in changes.keys():
+            logratings = changes['logratings']
+        else:
+            logratings = self.logratings
+        if 'days_jump' in changes.keys():
+            days_jump = changes['days_jump']
+        else:
+            days_jump = self.days_jump
+        if 'use_ratings' in changes.keys():
+            use_ratings = changes['use_ratings']
+        else:
+            use_ratings = self.use_ratings
+        if 'start_date' in changes.keys():
+            start_date = changes['start_date']
+        else:
+            start_date = self.date        
+        if 'first_occurance' in changes.keys():
+            self.first_occurance = changes['first_occurance']
+        else:
+            self.first_occurance = {}
+        self.default=default1
         self.conservaticism = conservaticism
         self.precision = precision
         self.weighting = weighting
@@ -68,18 +120,23 @@ class PythonReputationService(object):
         self.days_jump = days_jump
         self.use_ratings = use_ratings      
         self.date = start_date
+        return(0)
         
     ### This functions merely displays the parameters.
-    def get_parameters(self):
-        print("default=",self.default,",conservatism=",self.conservaticism,",precision=",self.precision,
-             ",weighting=",self.weighting,",fullnorm=",self.fullnorm,",liquid=",self.liquid,",logranks=",self.logranks,
-             ",temporal_aggregation=",self.temporal_aggregation,",logratings=",self.logratings,",days_jump=",self.days_jump,
-             "use_ratings=",self.use_ratings,"date=",self.date)
+    def get_parameters(self):              
+        return({'default': self.default, 'conservatism':self.conservaticism, 'precision':self.precision,
+               'weighting':self.weighting,'fullnorm':self.fullnorm, 'liquid':self.liquid,'logranks':self.logranks,
+               'aggregation':self.temporal_aggregation, 'logratings':self.logratings, 'days_jump':self.days_jump,
+               'use_ratings':self.use_ratings, 'date':self.date})
     ## Update date
     def set_date(self,newdate):
         self.our_date = newdate
     def clear_ranks(self):
         self.ranks = {}    
+        return(0)
+    def clear_ratings(self):
+        self.ratings = {}
+        return(0)
         
     def initialize_ranks(self,reputation=None,first_occurance = None):
         if reputation==None:
@@ -92,39 +149,54 @@ class PythonReputationService(object):
             self.first_occurance = first_occurance            
     
     def all_dates(self):
-        self.dates = []
-        i = 1
-        while i<=self.days_jump:
-            self.dates.append(self.date + timedelta(days=i))
+        i = 0
+        all_dates = []
+        while i<len(self.ratings):
+            if self.ratings[i]['time'] in all_dates:
+                pass
+            else:
+                all_dates.append(self.ratings[i]['time'])
             i+=1
+        self.dates = all_dates
+
        
-    def create_subset(self,data):
+    def convert_data(self,data):
+        daily_data = data[['from','type','to','value','weight','time','rating']].to_dict("records")
+        self.ratings = daily_data
         self.all_dates()
-        indexes = np.where(data['Date']==self.dates[0])[0]
-        i=1
-        while i<len(self.dates):
-            ola1 = np.where(data['Date']==self.dates[i])[0]
-            indexes = np.append(indexes,ola1)
+        i = 0
+        while i<len(daily_data):
+            daily_data[i]['from'] = str(daily_data[i]['from'])
+            daily_data[i]['to'] = str(daily_data[i]['to'])
             i+=1
-        mysubset = data.iloc[indexes]
-        if 'level_0' in mysubset:
-            del(mysubset['level_0'])        
-        mysubset = mysubset.reset_index()
-        daily_data = mysubset
-        ### Columns need to be renamed because my previous version is non-standardised.
-        daily_data = daily_data.rename(columns={"from":"From","to":"To"})
-        daily_data = daily_data.reset_index()
         return(daily_data)
         
-    def update_ranks(self):
+    def update_ranks(self,mydate):
         ### And then we iterate through functions. First we prepare arrays and basic computations.
-        start1 = time.time()
+        self.current_ratings = []
+        i=0
+        while i<len(self.ratings):
+            if self.ratings[i]['time'] == mydate:
+                self.current_ratings.append(self.ratings[i])
+            i+=1
         
-        array1 , dates_array, to_array, first_occurance = reputation_calc_p1(self.ratings,self.first_occurance,
+        i=0
+        problem = False
+        while i<len(self.current_ratings):
+            if (self.use_ratings==True and (not 'rating' in self.current_ratings[i].keys())):
+                problem=True
+            i+=1
+        if problem:
+            print("Ratings is set to True, but no ratings were given. Changing the setting to False")
+            self.use_ratings=False
+        
+        start1 = time.time()
+        array1 , dates_array, to_array, first_occurance = reputation_calc_p1(self.current_ratings,self.first_occurance,
                                                                              self.temporal_aggregation)  
+        
         self.first_occurance = first_occurance
         self.reputation = update_reputation(self.reputation,array1,self.default)
-        since = self.our_date - timedelta(days=self.days_jump)
+        since = self.date - timedelta(days=self.days_jump)
         ### And then update reputation.
         if self.logranks:
             new_reputation = calculate_new_reputation(array1,to_array,self.reputation,self.use_ratings,
@@ -136,19 +208,101 @@ class PythonReputationService(object):
                                                      liquid = self.liquid, logratings = self.logratings)
         ### In our case we take approach c.
         self.reputation = update_reputation_approach_d(self.first_occurance,self.reputation,new_reputation,since,
-                                                       self.our_date, self.default,self.conservaticism)
+                                                       self.date, self.default,self.conservaticism)
         
-    def update_ratings(self, ratings):
+        self.all_reputations[mydate] = self.reputation
+        return(0)
+        
+    def update_ratings(self, ratings, mydate):
+        i = 0
+        self.current_ratings = []
+        while i<len(ratings):
+            if ratings[i]['time'] == mydate:
+                self.current_ratings.append(ratings[i])
+            i+=1
         if ratings is None:
             print("No data detected")
-        else:
+        ### Below is not needed in our case.
+        #else:
+        #    self.current_ratings = ratings
+                     
+            
+    def put_ratings(self,ratings):
+        i = 0
+        while i<len(ratings):
+            ratings[i]['from'] = str(ratings[i]['from'])
+            ratings[i]['to'] = str(ratings[i]['to'])
+            i+=1
+        if self.ratings == {}:
             self.ratings = ratings
-    
-    def get_ranks(self):
-        print(self.reputation)
+        else:
+            self.ratings.append(ratings)
+        return(0)  
+        
+    def get_ranks(self,times):
+        if self.all_reputations == {}:
+            result = {}
+        else:
+            if times['date'] in list(self.all_reputations.keys()):
+                result = self.all_reputations[times['date']]
+            else:
+                result = {}
+        all_results = []
+        for k in result.keys():
+            all_results.append({'id':k,'rank':result[k]*100})  
+        return(0,all_results)
+        
     def add_time(self,addition=0):
         if addition==0:
             self.date = self.date + timedelta(days=self.days_jump)
         else:
             self.date = self.date + timedelta(days=addition)    
 
+    def get_ratings(self,times={}):
+        if times=={}:
+            return(0,self.ratings)
+        else:
+            all_ids = str(times['ids'])
+            since = times['since']
+            until = times['until']
+            results = []
+            i = 0
+            while i<len(self.ratings):
+                if self.ratings[i]['from'] in all_ids:
+                    if (self.ratings[i]['time'] >= since and self.ratings[i]['time'] <= until):
+                        results.append(self.ratings[i])
+                if self.ratings[i]['to'] in all_ids:
+                    if (self.ratings[i]['time'] >= since and self.ratings[i]['time'] <= until):
+                        results.append(self.ratings[i])                
+                i+=1
+            return(0,results)
+   
+    def put_ranks(self,dt1,mydict):
+        i = 0
+        while i<len(mydict):
+            mydict[i]['id'] = str(mydict[i]['id'])
+            mydict[i]['rank'] = mydict[i]['rank']/100
+            i+=1        
+        if dt1 in self.all_reputations:
+            myreps = self.all_reputations[dt1]
+        else:
+            myreps = {}
+        dict_values = mydict
+        i = 0
+        while i<len(dict_values):
+            the_id = dict_values[i]['id']
+            rank = dict_values[i]['rank']
+            myreps[the_id] = rank
+            i+=1
+        self.all_reputations[dt1] = myreps
+        if dt1 == max(self.all_reputations.keys()):
+            self.reputation = myreps
+        return(0)
+        
+    def __init__(self):
+        params = {'default':0.5, 'conservaticism': 0.5, 'precision': 0.01, 'weighting': True, 'fullnorm': True,
+         'liquid': True, 'logranks': True, 'temporal_aggregation': False, 'logratings': False, 'days_jump': 1,
+         'use_ratings': True, 'start_date': datetime.date(2018, 1, 1)}
+        self.all_reputations = {}
+        self.set_parameters(params)
+            
